416f00a9381705c0b548487d5b1d4ca5ea541b97
aver <66864263+AverWasTaken@users.noreply.github.com>
2024-01-28 10:11:56 -0800

    Update selection logic of a/b days

--- js/daysel.js
+++ js/daysel.js
@@ -1,28 +1,25 @@
 function updateDay() {
-    let knownBday = new Date('2024-01-23'); // Known "B day"
-    knownBday.setHours(0, 0, 0, 0); // Set time to 00:00:00.000
+    let knownAday = new Date('2024-01-25'); // Known "A day"
+    knownAday.setHours(0, 0, 0, 0); // Set time to 00:00:00.000
 
     let today = new Date();
     today.setHours(0, 0, 0, 0); // Set time to 00:00:00.000
 
     let daySpan = document.querySelector('.day');
 
-
-    // Calculate the difference in days from the known "B day"
-    let diffTime = today - knownBday;
+    // Calculate the difference in days from the known "A day"
+    let diffTime = today - knownAday;
     let diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
 
-    let dayType = diffDays % 2 === 0 ? 'B' : 'A';
+    // Determine day type based on difference in days, including weekends
+    // Since we're starting from an "A" day, an even difference means "A" day, odd means "B" day
+    let dayType = diffDays % 2 === 0 ? 'A' : 'B';
 
+    // Display logic for weekends
     if (today.getDay() === 0 || today.getDay() === 6) {
-        
-        if (dayType === 'A') {
-            daySpan.innerHTML = ` <br>It will be an ${dayType} day Monday`;
-        } else { 
-            daySpan.innerHTML = ` <br> It will be a ${dayType} day Monday`;
-        }
+        daySpan.innerHTML = `It will be ${dayType === 'A' ? 'an' : 'a'} ${dayType} day on Monday.`;
     } else {
-        
+        // Display logic for weekdays
         if (daySpan) {
             daySpan.innerHTML = `<span class="day">${dayType} Day</span>`;
         }
